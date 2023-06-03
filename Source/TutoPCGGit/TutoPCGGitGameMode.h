704feefe4dc4034ea08ee47d88735a5fe59f9888
Adrien Logut <Newsadrien@gmail.com>
2023-06-03 11:45:03 -0400

    Init Commit

---
+++ Source/TutoPCGGit/TutoPCGGitGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TutoPCGGitGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATutoPCGGitGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATutoPCGGitGameMode();
+};
+
+
+
